afb7bb2b631e65038e932ad20351aae812d92923
Kevin Nowland <kevin.nowland@gmail.com>
2023-03-19 19:46:50 -0400

    feat: put colorscheme into separte file

---
+++ lua/colors.lua
@@ -0,0 +1,34 @@
+-- color
+--[[
+vim.g.nord_disable_background = true
+vim.g.nord_contrast = true
+vim.g.nord_italic = false
+vim.cmd[[colorscheme nord]]
+--]]
+
+--[[
+vim.g.starry_disable_background = true
+vim.g.starry_italic_comments = true
+vim.cmd[[colorscheme darksolar]]
+--]]
+
+require('rose-pine').setup({
+	disable_background = true,
+	disable_italics = true,
+})
+vim.cmd('colorscheme rose-pine')
+
+--[[
+require('nightfox').setup({
+	options = {
+		transparent = true,
+	},
+})
+vim.cmd[[colorscheme terafox]]
+--]]
+
+--[[
+vim.g.everforest_transparent_background = true
+vim.cmd[[colorscheme everforest]]
+--]]
+
