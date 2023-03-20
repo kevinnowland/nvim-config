60e3714aeb7eda8df3a98db426423542ceda1980
Kevin Nowland <kevin.nowland@awarehq.com>
2023-03-20 09:47:28 -0400

    style: spaces to tabs

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -10,21 +10,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local opts = { buffer = ev.buf }
 		vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
 		vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<space>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
 		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
 		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 		vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-		
 	end,
 })
 
 -- format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  buffer = buffer,
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end,
+	buffer = buffer,
+	callback = function()
+		vim.lsp.buf.format { async = false }
+	end,
 })
